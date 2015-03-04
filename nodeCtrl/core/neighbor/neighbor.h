bc80105196a71455deaa692ced690545376128d6
BunneyDude <mark.bunneyjr@gmail.com>
2015-03-03 21:17:13 -0800

    added comments describing the struct fields for neighborEntry

--- nodeCtrl/core/neighbor/neighbor.h
+++ nodeCtrl/core/neighbor/neighbor.h
@@ -4,22 +4,25 @@
 #include <stdint.h>
 #include "neighbor-config.h"
 
+//neighborEntry holds information needed to communicate w/ adjacent nodes
 #if PACK_STRUCT == 1
 struct neighborEntry{
-	uint16_t shNodeID;
-	uint16_t shLQE : 12;
-	uint8_t nodeID : 2;
-	uint8_t networkID : 2;
+	uint16_t shNodeID; //single-hop node ID; each node has a unique ID
+	uint16_t shLQE : 12; //single-hop Link Quality Estimator; more details determined by the active LQE method
+	uint8_t radioID : 2; //which radio this entry is for. Each node can support 4 radio links (don't have to all be different radios)
+	uint8_t networkID : 2; //used for jumping between different RF networks. 
 };
 #else
 struct neighborEntry{
 	uint16_t shNodeID;
-	uint8_t nodeID;
-	uint8_t networkID;
 	uint16_t shLQE;
+	uint8_t networkID;
+	uint8_t radioID;
 };
 #endif
 
+//Determine what we're trying to build for. Currently used to omit printfs
+//might move this out to a more global file
 #ifdef LINUX
 #pragma message("Linux target")
 #else
@@ -29,8 +32,5 @@ struct neighborEntry{
 	#pragma error("No target defined")
 	#endif
 #endif
-
-
-
 
 #endif
