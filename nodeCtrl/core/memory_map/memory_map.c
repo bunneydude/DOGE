93b13b98a71ee438e581490e976cb222c465b0df
BunneyDude <mark.bunneyjr@gmail.com>
2015-03-26 23:41:51 -0700

    subtract base of address space before calling handler method

--- nodeCtrl/core/memory_map/memory_map.c
+++ nodeCtrl/core/memory_map/memory_map.c
@@ -15,19 +15,19 @@ uint8_t check_mm_space(uint8_t rw, uint8_t addr, uint8_t* data, uint8_t mask)
 	if( addr < MM_FUNCTION_BAR ){
 		return 1;
 	}else if( (addr >= MM_GPIO_BASE) && (addr < (MM_GPIO_BASE + MM_GPIO_SIZE)) ){
-		(*memoryMapRegionMethods.gpio_handler)(rw, addr, data, mask);
+		(*memoryMapRegionMethods.gpio_handler)(rw, addr - MM_GPIO_BASE, data, mask);
 		return 2;
 
 	}else if( (addr >= MM_ADC_BASE) && (addr < (MM_ADC_BASE + MM_ADC_SIZE)) ){
-		(*memoryMapRegionMethods.adc_handler)(rw, addr, data, mask);
+		(*memoryMapRegionMethods.adc_handler)(rw, addr - MM_ADC_BASE, data, mask);
 		return 3;
 
 	}else if( (addr >= MM_UART_BASE) && (addr < (MM_UART_BASE + MM_UART_SIZE)) ){
-		(*memoryMapRegionMethods.uart_handler)(rw, addr, data, mask);
+		(*memoryMapRegionMethods.uart_handler)(rw, addr - MM_UART_BASE, data, mask);
 		return 4;
 	
 	}else if( (addr >= MM_DSP_BASE) && (addr < (MM_DSP_BASE + MM_DSP_SIZE)) ){
-		(*memoryMapRegionMethods.dsp_handler)(rw, addr, data, mask);
+		(*memoryMapRegionMethods.dsp_handler)(rw, addr - MM_DSP_BASE, data, mask);
 		return 5;
 
 	}
