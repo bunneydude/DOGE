c84359f8005dbaa083b139e655bcd6ed83fe34dc
BunneyDude <mark.bunneyjr@gmail.com>
2015-04-05 13:27:59 -0700

    increased buffers to fit the src and dst ids; commented out debug code so received packets go through the full flow

--- nodes/edisonArduino/radio_proxy.ino
+++ nodes/edisonArduino/radio_proxy.ino
@@ -5,8 +5,6 @@
 #include <IPCBuffer.h>
 #include <stdint.h>
 
-
-
 // state
 uint8_t hbt_output = 0;
 uint8_t temp=0;
@@ -16,10 +14,6 @@ uint8_t data = 0;
 uint8_t state = 0;
 Protocol myProtocol(1);
 
-//TTYUARTClass test1 = Serial1;
-
-//IPCBuffer sketchCmdBuffer(1);
-//IPCBuffer sketchRxBuffer(2);
 IPCBuffer proxyCmdBuffer(3);
 IPCBuffer proxyRxBuffer(4);
 
@@ -29,151 +23,112 @@ uint8_t printTime = 0x0;
 
 void setup() {                
 
-//   system("telnetd -l /bin/sh"); //set up Telnet
-//   system("ifconfig eth0 169.254.1.1 netmask 255.255.0.0 up");
+  //   system("telnetd -l /bin/sh"); //set up Telnet
+  //   system("ifconfig eth0 169.254.1.1 netmask 255.255.0.0 up");
 
-   //initialize pins
-   pinMode(HBT_LED, OUTPUT);   
-   Serial.begin(SERIAL_BAUD);
-   Serial1.begin(9600);
+  //initialize pins
+  pinMode(HBT_LED, OUTPUT);   
+  Serial.begin(SERIAL_BAUD);
+  Serial1.begin(9600);
 
-   while(Serial1.available()){ //flush
-      temp = Serial1.read();
-   }
+  while(Serial1.available()){ //flush
+    temp = Serial1.read();
+  }
 
-   digitalWrite(HBT_LED, 0x1);
-   hbt_output = 0x1;
+  digitalWrite(HBT_LED, 0x1);
+  hbt_output = 0x1;
 
-//   sketchCmdBuffer.open(CMD_BUFFER_LENGTH, CMD_BUFFER_WIDTH);
-//   sketchRxBuffer.open(RX_BUFFER_LENGTH, RX_BUFFER_WIDTH);
+  proxyCmdBuffer.open(CMD_BUFFER_LENGTH, CMD_BUFFER_WIDTH);
+  proxyRxBuffer.open(RX_BUFFER_LENGTH, RX_BUFFER_WIDTH);
 
-   proxyCmdBuffer.open(CMD_BUFFER_LENGTH, CMD_BUFFER_WIDTH);
-   proxyRxBuffer.open(RX_BUFFER_LENGTH, RX_BUFFER_WIDTH);
-   
-   delay(2000);
+  delay(250);
+  digitalWrite(HBT_LED, hbt_output^=0x01);  
+  delay(250);
+  digitalWrite(HBT_LED, hbt_output^=0x01);  
+  delay(250);
+  digitalWrite(HBT_LED, hbt_output^=0x01);  
+  delay(250);
 }
 
 
 void loop() {
 
-   //  if(1 == task_hbt){
-   //    task_hbt = TASK_HBT_PERIOD;
+  digitalWrite(HBT_LED, hbt_output^=0x01);  
+  firstStamp = micros();   
+  //   sketch_respond(sketchCmdBuffer, sketchRxBuffer);
+  proxy_respond(proxyCmdBuffer, proxyRxBuffer); 
+  lastStamp = micros();
 
-   //  }    
+  if(printTime == 0x1){
+    Serial.println("Time stamp uartTx:");
+    Serial.println(lastStamp - firstStamp); 
+    Serial.println(firstStamp);
+    Serial.println(lastStamp);
+    Serial.println(" ");
+    printTime = 0x0;
+  }
 
-   digitalWrite(HBT_LED, hbt_output^=0x01);  
-firstStamp = micros();   
-//   sketch_respond(sketchCmdBuffer, sketchRxBuffer);
-   proxy_respond(proxyCmdBuffer, proxyRxBuffer); 
-lastStamp = micros();
-
-if(printTime == 0x1){
-  Serial.println("Time stamp uartTx:");
-  Serial.println(lastStamp - firstStamp); 
-  Serial.println(firstStamp);
-  Serial.println(lastStamp);
-  Serial.println(" ");
-  printTime = 0x0;
 }
-
-//delay(1000); 
-}
-/*
-void sketch_respond(IPCBuffer buf, IPCBuffer resp){
-   if(buf.available()){
-      printTime = 0x1;
-      uint8_t rxBuffer[5];
-      uint8_t response[5];   
-      uint8_t index = 0;
-      uint8_t i = 0;
-      uint8_t sendResponse = 0;
-
-//get packet from ipc buffer
-      while(buf.available() < 2); //wait for 2 bytes
-      buf.read(&rxBuffer[index++]); //cmd
-      buf.read(&rxBuffer[index++]); //size
-
-      for(i=0; i<rxBuffer[1]; i++){ //get rest of bytes
-         while(buf.available() == 0); //wait for a byte
-         buf.read(&rxBuffer[index++]);
-      }
-
-      while(buf.available() == 0); //wait for checksum
-      buf.read(&rxBuffer[index++]);
-
-//parse packet
-      sendResponse = myProtocol.parse_packet(rxBuffer, response);
-
-//send response to ipc buffer
-      if(sendResponse == 1){
-         for(i=0; i<response[1]+3; i++){
-            resp.write(&response[i]);
-         }
-      }
-   } 
-}*/
 
 void proxy_respond(IPCBuffer buf, IPCBuffer resp){
-//   if(buf.available()){
-      printTime = 0x1;
-      uint8_t rxBuffer[5];
-      uint8_t response[5];   
-      uint8_t index = 0;
-      uint8_t i = 0;
+  printTime = 0x1;
+  uint8_t rxBuffer[7];
+  uint8_t response[7];   
+  uint8_t index = 0;
+  uint8_t i = 0;
 
-//get message from cmd buffer
-      //while(buf.available() < 2); //wait for 2 bytes
-      buf.read(&rxBuffer[index++]); //cmd
-      buf.read(&rxBuffer[index++]); //size
+  //get message from cmd buffer
+  buf.read(&rxBuffer[index++]); //srcID
+  buf.read(&rxBuffer[index++]); //dstID
+  buf.read(&rxBuffer[index++]); //cmd
+  buf.read(&rxBuffer[index++]); //size
 
-      for(i=0; i<rxBuffer[1]; i++){ //get rest of bytes
-         //while(buf.available() == 0); //wait for a byte
-         buf.read(&rxBuffer[index++]);
-      }
+  for(i=0; i<rxBuffer[3]; i++){ //get rest of bytes
+    buf.read(&rxBuffer[index++]);
+  }
 
-      //while(buf.available() == 0); //wait for checksum
-      buf.read(&rxBuffer[index++]);
+  buf.read(&rxBuffer[index++]); //get checksum
 
 
 #ifdef PROXY_MSP
-//send command to proxy mcu
-      for(i=0; i<rxBuffer[1]+3; i++){
-         myProtocol._uartTxBuffer.write(rxBuffer[i]);
-      }
+  //send command to proxy mcu
+  for(i=0; i<rxBuffer[3]+5; i++){
+    myProtocol._uartTxBuffer.write(rxBuffer[i]);
+  }
 
-      myProtocol.uartTx();
+  myProtocol.uartTx();
 
-//get response from proxy mcu
-//start rx
-      index = 0;
-      while(Serial1.available() < 2); //wait for 2 bytes
-      
-//first bytes rx      
-      response[index++] = Serial1.read(); //cmd
-      response[index++] = Serial1.read(); //size     
+  //get response from proxy mcu
+  //start rx
+  index = 0;
+  while(Serial1.available() < 4); //wait for 4 bytes
 
-//mid rx
-      for(i=0; i<response[1]; i++){ //get rest of bytes
-         while(Serial1.available() == 0); //wait for a byte
-         response[index++] = Serial1.read();
-      }
+  //first bytes rx      
+  response[index++] = Serial1.read(); //srcID
+  response[index++] = Serial1.read(); //dstID 
+  response[index++] = Serial1.read(); //cmd
+  response[index++] = Serial1.read(); //size     
 
-      while(Serial1.available() == 0); //wait for checksum
-      response[index++] = Serial1.read();
-//end rx
+  //mid rx
+  for(i=0; i<response[3]; i++){ //get rest of bytes
+    while(Serial1.available() == 0); //wait for a byte
+    response[index++] = Serial1.read();
+  }
+
+  while(Serial1.available() == 0); //wait for checksum
+  response[index++] = Serial1.read();
+  //end rx
 #else
-//simulate data
-  myProtocol.form_packet(response, ACK, 0, FEATURE_WIP);
+  //simulate data
+  myProtocol.form_packet(&response[2], ACK, 0, FEATURE_WIP);
+  response[0] = 2;
+  response[1] = 1;
 #endif
 
-
-//pre tx - start rx = 441us
-//start rx - first bytes rx = 14220
-//mid rx - end rx = 20us
-
-//write response to ipc buffer
-      for(i=0; i<response[1]+3; i++){
-         resp.write(&response[i]);
-      }
-//   }//end if availabe 
+  //write response to ipc buffer
+  for(i=0; i<response[3] + 5; i++){
+    resp.write(&response[i]);
+  }
 }
+
+
--- nodes/msp430g2553/proxy_nodeCtrl/proxy_nodeCtrl.ino
+++ nodes/msp430g2553/proxy_nodeCtrl/proxy_nodeCtrl.ino
@@ -21,7 +21,8 @@ uint8_t rxData[DATA_LENGTH];
 #define MY_NODE_ID 0x1
 #define NODECTRL_VERSION 1
 
-uint8_t nrf_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+uint8_t nrf_address[5] = {
+  0xD7,0xD7,0xD7,0xD7,0xD7};
 uint8_t nrfRXAttempts = 0;
 
 Protocol myProtocol(0);
@@ -33,18 +34,18 @@ uint8_t e2eSrcID = 0;
 // -----------------------------------------------------------------------------
 //functions for nRF
 void nrf24_ce_digitalWrite(uint8_t state){
-      digitalWrite(RADIO_NRF_CE,state);
+  digitalWrite(RADIO_NRF_CE,state);
 }
 
 void nrf24_csn_digitalWrite(uint8_t state){
-      digitalWrite(RADIO_NRF_CSN,state);
+  digitalWrite(RADIO_NRF_CSN,state);
 }
 
 uint8_t spi_transfer(uint8_t tx)
 {
-    uint8_t rx = 0;    
-    rx = SPI.transfer(tx);
-    return rx;
+  uint8_t rx = 0;    
+  rx = SPI.transfer(tx);
+  return rx;
 }
 
 uint8_t nrf_read_timeout(uint16_t timeout){
@@ -55,7 +56,7 @@ uint8_t nrf_read_timeout(uint16_t timeout){
     }
   }
   return 0;    
-  }
+}
 
 // Main example
 
@@ -65,18 +66,18 @@ void setup()
   digitalWrite(RADIO_NRF_CE, LOW);     
   pinMode(RADIO_NRF_CSN, OUTPUT);
   digitalWrite(RADIO_NRF_CSN, HIGH); 
-  
+
   myProtocol.serial_registers[SREG_NODE_ID] = MY_NODE_ID;
   myProtocol.serial_registers[SREG_TX_PWR] = 0;
   myProtocol.serial_registers[SREG_RSSI] = 0;
   myProtocol.serial_registers[SREG_LQI] = 0;
   myProtocol.serial_registers[SREG_TEMPERATURE] = 0;
   myProtocol.serial_registers[SREG_PING] = 0;
-  
+
   Radio.begin(0x01, CHANNEL_1, POWER_MAX);
-  
+
 #ifdef DUAL_RADIO
- SPI.begin(); 
+  SPI.begin(); 
   nrf24_init();
   nrf24_config(2, DATA_LENGTH);
   nrf24_tx_address(nrf_address);
@@ -84,12 +85,12 @@ void setup()
 #endif
 
   Serial.begin(9600);
-  
+
   uint8_t temp;
-   while(Serial.available()){ //flush rx pipe
-      temp = Serial.read();
-   }
-   
+  while(Serial.available()){ //flush rx pipe
+    temp = Serial.read();
+  }
+
 
   pinMode(RED_LED, OUTPUT);
   digitalWrite(RED_LED, hbt_output);
@@ -100,65 +101,72 @@ void setup()
 void loop(){
 
   while(!serial_receive(rxData)); //wait for data
-
-  if(rxData[1] == MY_NODE_ID){
-    //try to handle it locally
-    if(myProtocol.parse_packet(&rxData[2], &txData[2])){
-      txData[0] = MY_NODE_ID;
-      txData[1] = rxData[0];
+/*
+    //echo it back
+  for(i=0; i<rxData[3] + 5; i++){ //size at [3] plus 5 non-payload bytes
+    myProtocol._uartTxBuffer.write(rxData[i]);
+  }
+  myProtocol.uartTx();
+*/
+  
+    if(rxData[1] == MY_NODE_ID){
+      //try to handle it locally
+      if(myProtocol.parse_packet(&rxData[2], &txData[2])){
+        txData[0] = MY_NODE_ID;
+        txData[1] = rxData[0];
+        for(i=0; i<txData[3] + 5; i++){ //size at [3] plus 5 non-payload bytes
+          myProtocol._uartTxBuffer.write(txData[i]);
+        }
+        myProtocol.uartTx();
+      }
+    }else{ //forward it
+      e2eSrcID = rxData[0]; //store the original source ID
+      
+      Radio.transmit(0x2, rxData, DATA_LENGTH);
+  
+      //Make sure radio is ready to receive
+      while (Radio.busy());
+  
+      // Turn on the receiver and listen for incoming data. Timeout after 1 seconds.
+      if (Radio.receiverOn(txData, sizeof(txData), 1000) > 0){
+        digitalWrite(RED_LED, hbt_output ^= 0x1);
+  
+        //update radio info
+        myProtocol.serial_registers[SREG_RSSI] = Radio.getRssi();
+        myProtocol.serial_registers[SREG_LQI] = Radio.getLqi();     
+  
+      }else{ //timeout, send error message
+          myProtocol.form_packet(&txData[2], NACK, 0, TIMEOUT);     
+          txData[0] = MY_NODE_ID;
+          txData[1] = e2eSrcID;
+      }
       for(i=0; i<txData[3] + 5; i++){ //size at [3] plus 5 non-payload bytes
         myProtocol._uartTxBuffer.write(txData[i]);
       }
-      myProtocol.uartTx();
-    }
-  }else{ //forward it
-    e2eSrcID = rxData[0]; //store the original source ID
-    
-    Radio.transmit(0x2, rxData, DATA_LENGTH);
-
-    //Make sure radio is ready to receive
-    while (Radio.busy());
-
-    // Turn on the receiver and listen for incoming data. Timeout after 1 seconds.
-    if (Radio.receiverOn(txData, sizeof(txData), 1000) > 0){
-      digitalWrite(RED_LED, hbt_output ^= 0x1);
-
-      //update radio info
-      myProtocol.serial_registers[SREG_RSSI] = Radio.getRssi();
-      myProtocol.serial_registers[SREG_LQI] = Radio.getLqi();     
-
-    }else{ //timeout, send error message
-        myProtocol.form_packet(&txData[2], NACK, 0, TIMEOUT);     
-        txData[0] = MY_NODE_ID;
-        txData[1] = e2eSrcID;
-    }
-    for(i=0; i<txData[3] + 5; i++){ //size at [3] plus 5 non-payload bytes
-      myProtocol._uartTxBuffer.write(txData[i]);
-    }
-    myProtocol.uartTx();      
-  }//end forward  
+      myProtocol.uartTx();      
+    }//end forward  
 }//end main loop
 
 uint8_t serial_receive(uint8_t* buf){
-   if(Serial.available()){
-      uint8_t rxBuffer[5];  
-      uint8_t index = 0;
-      uint8_t i = 0;
+  if(Serial.available()){ 
+    uint8_t index = 0;
+    uint8_t i = 0;
 
-      while(Serial.available() < 4); //wait for 4 bytes (srcID, dstID, cmd, size)
-      for(i=0; i<4; i++){
-        rxBuffer[index++] = Serial.read();
-      }
+    while(Serial.available() < 4); //wait for 4 bytes (srcID, dstID, cmd, size)
+    for(i=0; i<4; i++){
+      buf[index++] = Serial.read();
+    }
 
-      while(Serial.available() < rxBuffer[3]); //wait for rest of bytes (payload)
-      for(i=0; i<rxBuffer[3]; i++){ //get rest of bytes
-         rxBuffer[index++] = Serial.read();
-      }
+    while(Serial.available() < buf[3]); //wait for rest of bytes (payload)
+    for(i=0; i<buf[3]; i++){ //get rest of bytes
+      buf[index++] = Serial.read();
+    }
 
-      while(Serial.available() == 0); //wait for checksum
-      rxBuffer[index++] = Serial.read();
+    while(Serial.available() == 0); //wait for checksum
+    buf[index++] = Serial.read();
 
-      return 1;
-   }
-   return 0;
+    return 1;
+  }
+  return 0;
 }
+
