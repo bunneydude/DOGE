f405bc572ccf24124cf43c8c883115cf7ac8479e
Daveman <dave@rulostapp.com>
2015-05-10 16:09:12 -0700

    Updating preset nte nodes list within doge.conf.globals to align with new practice of reserving Edison as node 1, and providing the 9 additional nodes for use in our tic-tac-toe demo setup

--- linux/doge/conf/globals.py
+++ linux/doge/conf/globals.py
@@ -10,20 +10,20 @@ config = defaultdict()
 # Debug Flag (project-wide)
 config['debug'] = True
 config['debug_no_sketch'] = True #Don't send data to sketch via IPCBuffer
-config['debug_test_network'] = True #Use a preset network for neighbor/routing edges
+config['debug_test_network'] = False #Use a preset network for neighbor/routing edges
 
 
 # Preset Neighbor Table Nodes
 nte_nodes = [
-             {'node_id': 1, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-             {'node_id': 2, 'mcu_name': 'lpc812',      'mm_num': 1},
-             {'node_id': 3, 'mcu_name': 'msp430g2553', 'mm_num': 1},
+             {'node_id': 2, 'mcu_name': 'msp430g2553', 'mm_num': 1},
+             {'node_id': 3, 'mcu_name': 'lpc812',      'mm_num': 1},
              {'node_id': 4, 'mcu_name': 'msp430g2553', 'mm_num': 1},
              {'node_id': 5, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-             {'node_id': 6, 'mcu_name': 'lpc812',      'mm_num': 1},
-             {'node_id': 7, 'mcu_name': 'msp430g2553', 'mm_num': 1},
+             {'node_id': 6, 'mcu_name': 'msp430g2553', 'mm_num': 1},
+             {'node_id': 7, 'mcu_name': 'lpc812',      'mm_num': 1},
              {'node_id': 8, 'mcu_name': 'msp430g2553', 'mm_num': 1},
              {'node_id': 9, 'mcu_name': 'msp430g2553', 'mm_num': 1},
+             {'node_id': 10, 'mcu_name': 'msp430g2553', 'mm_num': 1},
             ]
 config['preset_nte_nodes'] = nte_nodes
 
