efc650a53da125b2cbcbb23a9c1f092fd148407f
Mario Escalante <mario.s.escalante@gmail.com>
2015-05-09 18:28:08 -0700

    Add packet crc calculation to proxy nodeCtrl

--- nodes/msp430g2553/proxy_nodeCtrl/proxy_nodeCtrl.ino
+++ nodes/msp430g2553/proxy_nodeCtrl/proxy_nodeCtrl.ino
@@ -104,6 +104,7 @@ void loop(){
   if(rxPacket.hdr.dst == MY_NODE_ID){ //ACK back same address and data
     application_form_packet(txAppPacket, &txAttr, CMD_ACK, rxAppPacket->addr, rxAppPacket->data, NULL);
     link_layer_form_packet(&txPacket, &txAttr, RAW_PACKET, MY_NODE_ID, rxPacket.hdr.src, MY_NODE_ID, rxPacket.hdr.shSrc);  
+    add_packet_crc(&txPacket);
   }
   else{ //forward it
     copy_doge_packet(&txPacket, &rxPacket);
@@ -121,6 +122,7 @@ void loop(){
     else{ //timeout, send error message
       application_form_packet(txAppPacket, &txAttr, CMD_NACK, rxAppPacket->addr, ERR_TIMEOUT, NULL);
       link_layer_form_packet(&txPacket, &txAttr, RAW_PACKET, MY_NODE_ID, rxPacket.hdr.src, MY_NODE_ID, rxPacket.hdr.shSrc); 
+      add_packet_crc(&txPacket);
     }
   }//end forward
   serial_transmit((uint8_t*)(&txPacket), RAW_PACKET_TOTAL_SIZE(&txPacket), 1);
