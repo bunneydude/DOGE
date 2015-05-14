15f9e1f13b2d1faae23835264d5aecff7abbe3f0
BunneyDude <mark.bunneyjr@gmail.com>
2015-05-14 05:22:58 -0700

    added rest of the routes

--- linux/doge/scripts/demo_unified.py
+++ linux/doge/scripts/demo_unified.py
@@ -6,16 +6,6 @@ import sys
 import json
 import time
 
-
-star_nte = [
-#             {'node_id': 2, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-#             {'node_id': 3, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-             {'node_id': 4, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-             {'node_id': 5, 'mcu_name': 'msp430g2553', 'mm_num': 1},
-            ]
-
-config['preset_nte_nodes'] = star_nte
-
 config['debug'] =True
 config['debug_test_network'] = True
 
