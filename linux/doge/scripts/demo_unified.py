73b8687d4997a228bae0825dd48f35525bcb9026
bunneydude <mark.bunneyjr@gmail.com>
2015-05-15 02:40:08 +0000

    changed time in demo_unified to respond to webserver faster; added global log level variable

--- linux/doge/scripts/demo_unified.py
+++ linux/doge/scripts/demo_unified.py
@@ -12,25 +12,26 @@ config['debug_test_network'] = True
 networkSocket, routingProcessor, rootNode = rp_setup()
 plotSocket = plot_setup()
 
-#get node objects
-for node in routingProcessor.networkNodes:
-   print("Have node {0}".format(routingProcessor.networkNodes[node].get_nodeID()))
+networkNodes = routingProcessor.networkNodes
 
-node10 = routingProcessor.networkNodes[10]
-node10.to_s()
-node10.pull("networkConfig")
-rootNode.mask_neighbor()
-rootNode.mask_neighbor(nodeID=2, action="unmask")
+#get node objects
+for node in networkNodes.itervalues():
+#   print("Have node {0}".format(node.get_nodeID()))
+   node.mask_neighbor(action="unmask")
+
+node10 = networkNodes[10]
+node9 = networkNodes[9]
 
 plotData = []
 
 while True:
    #handle any network requests (blocking 1 second wait)
-   handle_rp_request(networkSocket, routingProcessor)
+   for _ in range(0,4):
+      handle_rp_request(networkSocket, routingProcessor)
 
    plotData = []
-   plotData.append([int(time.time()), 1])#node10.get_rssi()])
+   plotData.append([int(time.time()), node10.get_rssi()])
    print "Send to plot"
    plotSocket.emit('update', json.dumps(plotData))
    time.sleep(1)
-   node10.pull("networkConfig")
+   node9.pull("networkConfig")
