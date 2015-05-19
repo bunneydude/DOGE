9405cbefd8ae1cdf56ae9d6adb6095fc8c81a9ec
tkmaker <root@ubilinux.attlocal.net>
2015-05-19 04:12:26 +0000

    Removed stale scripts and code in webserver dir

--- linux/webserver/setup.sh
+++ linux/webserver/setup.sh
@@ -1,4 +1,4 @@
 #required node modules: webserver/node_modules
 #angular    express    ip         request    socket.io  vis
 
-npm install express request socket.io vis
+npm install express ionic request socket.io vis
