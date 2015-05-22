fac151c7fe42c7ddc787da9cbe5fa687919b2f1c
Daveman <dave@rulostapp.com>
2015-05-21 21:02:14 -0700

    Fixing minor typo in webserver setup script

--- linux/webserver/setup.sh
+++ linux/webserver/setup.sh
@@ -1,4 +1,4 @@
 #required node modules: webserver/node_modules
-angular    express    request    socket.io    vis
+#angular    express    request    socket.io    vis
 
 npm install express request socket.io vis
